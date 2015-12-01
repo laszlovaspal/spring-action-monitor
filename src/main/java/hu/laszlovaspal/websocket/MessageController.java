18c1612a44031098a2628a3db88f5015ce7bd676
Laszlo Vaspal <laszlo.vaspal@gmail.com>
2015-12-01 22:49:59 +0100

    Add SLF4J and Logback for logging

--- src/main/java/hu/laszlovaspal/websocket/MessageController.java
+++ src/main/java/hu/laszlovaspal/websocket/MessageController.java
@@ -1,5 +1,7 @@
 package hu.laszlovaspal.websocket;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
@@ -9,12 +11,16 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class MessageController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(MessageController.class);
+
     @Autowired
     private SimpMessagingTemplate messagingTemplate;
 
     @MessageMapping("/notify")
     @SendTo("/topic/db")
     public Message notifyClientOfDbChanges() {
+
+        LOGGER.info("sending message through websocket");
 
         for (int i = 0; i < 10; i++) {
             messagingTemplate.convertAndSend("/topic/db", new Message("cucc " + i));
